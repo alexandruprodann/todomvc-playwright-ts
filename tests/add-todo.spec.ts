c55cc43354bf95e72561abd0fc3a5e6e11e183ca
Alexandru Prodan <alexprodan711@gmail.com>
2025-04-15 22:26:40 +0300

    Remove unused page argument from tests

--- tests/add-todo.spec.ts
+++ tests/add-todo.spec.ts
@@ -11,31 +11,31 @@ test.describe('Tests for adding todos', () => {
 		todoPage = new TodoPage(page);
 	});
 
-	test('should add a single todo item', async ({ page }) => {
+	test('should add a single todo item', async () => {
 		await todoPage.addTodo(TODO_ITEMS[randomIndex]);
 		await expect(todoPage.todoItemLabelByText(TODO_ITEMS[randomIndex])).toBeVisible();
 	});
 
-	test('should add multiple todo items', async ({ page }) => {
+	test('should add multiple todo items', async () => {
 		for (const todoItem of TODO_ITEMS) {
 			await todoPage.addTodo(todoItem);
 			await expect(todoPage.todoItemLabelByText(todoItem)).toBeVisible();
 		}
 	});
 
-	test('should trim whitespace from entered text', async ({ page }) => {
+	test('should trim whitespace from entered text', async () => {
         let randomTodoItem = TODO_ITEMS[randomIndex]
         let toDoItemWithWhitespace: string = '    ' + randomTodoItem
 		await todoPage.addTodo(toDoItemWithWhitespace);
 		await expect(todoPage.todoItemLabelByText(randomTodoItem)).toBeVisible();
 	});
 
-    test('should not add a todo with only whitespace', async ({ page }) => {
+    test('should not add a todo with only whitespace', async () => {
         await todoPage.addTodo('     ');
         await expect(todoPage.todoItemLabel()).not.toBeVisible();
     });
 
-	test('should not add an empty todo', async ({ page }) => {
+	test('should not add an empty todo', async () => {
 		await todoPage.addTodo('');
 		await expect(todoPage.todoItemLabel()).not.toBeVisible();
 	});
