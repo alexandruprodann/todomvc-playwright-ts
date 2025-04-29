085793b2cefbc3b29355999356bb3bc087779ea0
Alexandru Prodan <alexprodan711@gmail.com>
2025-04-29 11:15:43 +0300

    Modify tests to follow best practices by checking visible style instead of class

--- tests/complete-todo.spec.ts
+++ tests/complete-todo.spec.ts
@@ -18,34 +18,34 @@ test.describe('Tests for marking todos complete/incomplete', () => {
 
     test('should mark a single todo as completed @smoke', async () => {
         await todoPage.checkTodo(randomTodo);
-        await expect(todoPage.todoItemByText(randomTodo)).toHaveClass(completed);
+        await expect(todoPage.todoItemLabelByText(randomTodo)).toHaveCSS('text-decoration', /line-through/);
     });
 
     test('should mark multiple todos as completed', async () => {
         for (const todoItem of TODO_ITEMS) {
 			await todoPage.checkTodo(todoItem);
-            await expect(todoPage.todoItemByText(todoItem)).toHaveClass(completed);
+            await expect(todoPage.todoItemLabelByText(todoItem)).toHaveCSS('text-decoration', /line-through/);
 		}
     });
 
     test('should uncheck a completed todo @smoke', async () => {
         await todoPage.checkTodo(randomTodo);
-        await expect(todoPage.todoItemByText(randomTodo)).toHaveClass(completed);
+        await expect(todoPage.todoItemLabelByText(randomTodo)).toHaveCSS('text-decoration', /line-through/);
         await todoPage.uncheckTodo(randomTodo);
-        await expect(todoPage.todoItemByText(randomTodo)).not.toHaveClass(completed);
+        await expect(todoPage.todoItemLabelByText(randomTodo)).not.toHaveCSS('text-decoration', /line-through/);
     });
 
     test('should update the counter when todos are completed', async () => {
         await todoPage.checkTodo(randomTodo);
-        await expect(todoPage.todoItemByText(randomTodo)).toHaveClass(completed);
+        await expect(todoPage.todoItemLabelByText(randomTodo)).toHaveCSS('text-decoration', /line-through/);
         expect(await todoPage.getTodoItemCount()).toBeLessThan(TODO_ITEMS.length);
     });
 
     test('should update the counter when todos are uncompleted', async () => {
         await todoPage.checkTodo(randomTodo);
-        await expect(todoPage.todoItemByText(randomTodo)).toHaveClass(completed);
+        await expect(todoPage.todoItemLabelByText(randomTodo)).toHaveCSS('text-decoration', /line-through/);
         await todoPage.uncheckTodo(randomTodo);
-        await expect(todoPage.todoItemByText(randomTodo)).not.toHaveClass(completed);
+        await expect(todoPage.todoItemLabelByText(randomTodo)).not.toHaveCSS('text-decoration', /line-through/);
         expect(await todoPage.getTodoItemCount()).toBe(TODO_ITEMS.length);
     });
 });
